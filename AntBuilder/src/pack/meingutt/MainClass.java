c56085de1e9239ac6e71c2225bed14c58a70fb3e
KorolER <elizavetakorol2005@gmail.com>
2024-10-27 20:37:38 +0300

    wip

--- AntBuilder/src/pack/meingutt/MainClass.java
+++ AntBuilder/src/pack/meingutt/MainClass.java
@@ -1,7 +1,13 @@
 package pack.meingutt;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+
 public class MainClass {
+	static Logger logger = Logger.getLogger(MainClass.class);
+
 	public static void main(String[] args) {
-		System.out.println("Test");
+		BasicConfigurator.configure();
+		logger.info("Hello World");
 	}
 }
